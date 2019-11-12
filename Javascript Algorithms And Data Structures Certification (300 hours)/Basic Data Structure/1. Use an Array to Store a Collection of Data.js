d3fe0354d88732cc5d175cb12d0db8fea0468e3d
appyofficial <appy70890@gmail.com>
2019-11-12 15:21:06 +0800

    Basic Data Structure Updates

---
+++ Javascript Algorithms And Data Structures Certification (300 hours)/Basic Data Structure/1. Use an Array to Store a Collection of Data.js
@@ -0,0 +1,6 @@
+/*
+We have defined a variable called yourArray. Complete the statement by assigning an array of at least 5 elements in length to the yourArray variable. Your array should contain at least one string, one number, and one boolean.
+*/
+
+let yourArray; // change this line
+yourArray = [29930, "Appy", "Sharma", 29, true];
